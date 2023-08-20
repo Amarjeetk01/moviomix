35a27e020ae9905e9b209c34e8b851b052de81ed
Amarjeet Kumar <89585658+Amarjeetk01@users.noreply.github.com>
2023-08-20 09:55:10 +0530

    Update DetailsBanner.jsx

--- src/pages/home/detailBanner/DetailsBanner.jsx
+++ src/pages/home/detailBanner/DetailsBanner.jsx
@@ -48,7 +48,7 @@ const DetailsBanner = ({ video, crew }) => {
                         <ContentWrapper >
                             <div className="content " >
                                 <div className="left">
-                                    {data.poster_path ? (<Img className="posterImg" src={url.backdrop + data?.backdrop_path}></Img>) : (
+                                    {data.poster_path ? (<img className="posterImg" src={url.backdrop + data?.backdrop_path alt="posterImg"}/>) : (
                                         <img className="posterImg" src={PosterFallback} alt="posterImg"/>
                                     )}
 
