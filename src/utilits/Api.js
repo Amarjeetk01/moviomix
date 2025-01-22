42e474de2c9632491b683c1d192c2dd3fa7e71a4
Amarjeet Kumar <amarjeetkumarekma@gmail.com>
2025-01-23 01:58:22 +0530

    update

--- src/utilits/Api.js
+++ src/utilits/Api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TOKEN = process.env.REACT_APP_TMBD_TOKEN;
+// const TOKEN = process.env.REACT_APP_TMBD_TOKEN;
+const TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzNiNjE1NWNhMDQzNWZmMjhmOGIyMTUwYmI0ZDc5OCIsIm5iZiI6MTczNzU2NjMyNi41ODYsInN1YiI6IjY3OTEyODc2YmU5YTlhYTc4Mjc3NjIxNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R9h9KVGOQSisJJVyS4FxUtPt3341rVxSBQeZFKtoZNE";
 
 const headers = {
   Authorization: "Bearer " + TOKEN,
